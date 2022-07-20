04906f8286d80498f73c9319ae493380b98345c8
Marie-JG <99450393+Marie-JG@users.noreply.github.com>
2022-07-20 17:49:10 -0300

    Update AppServlet.java

--- cac-crud-java-web-22034-master/src/main/java/cac/crud22034/controlador/AppServlet.java
+++ cac-crud-java-web-22034-master/src/main/java/cac/crud22034/controlador/AppServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.Properties;
 
-@WebServlet(name = "AppServlet", urlPatterns = {"/app"})
+@WebServlet(name = "AppServlet", urlPatterns = {"/cac-crud-java-web-22034"})
 public class AppServlet extends HttpServlet {
 
     private Modelo model;
